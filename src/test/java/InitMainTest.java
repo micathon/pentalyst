4dace4ad0af39c0576fd5e0a953b70c4bb949fa5
Mike Hahn <hahnbytes@gmail.com>
2026-01-30 21:01:36 -0500

    Add files via upload

---
+++ src/test/java/InitMainTest.java
@@ -0,0 +1,18 @@
+package test.java;
+
+import org.junit.Test;
+import init.InitMain;
+
+import static org.junit.Assert.*;
+
+public class InitMainTest {
+
+  @Test
+  public void testFactorial() {
+    InitMain ex = new InitMain();
+    assertEquals(ex.factorial(4), 24);
+    assertEquals(ex.factorial(5), 120);
+    assertEquals(ex.factorial(1), 1);
+    assertEquals(ex.factorial(0), 1);
+  }
+}
