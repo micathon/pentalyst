d3df8e21df561c2387b10265df64b5bff9ac41ee
Mike Hahn <hahnbytes@gmail.com>
2026-02-03 21:25:03 -0500

    Add files via upload

--- src/main/java/com/app/pny.java
+++ src/main/java/com/app/pny.java
@@ -1,48 +1,74 @@
 package com.app;
 
+import iconst.IConst;
 import init.InitMain;
 import config.Config;
 
-public class pny {
+public class pny implements IConst {
 
 	public static void main(String[] args) {
 		InitMain initobj;
-		Config cfg;
+		Config cfg = null;
 		String fileName = "";
 		String filePath = "";
+		String fileMidPath;
+		String sep = "" + SEPCH;
+		String arg;
+		String sepWord = "";
+		int idx;
+		boolean argExists = true;
 		boolean isUnitTest = false;
 		boolean isMain = false;
 		boolean isRunTest = false;
 
-		cfg = new Config(false);
-		if (args.length == 1) {
-			filePath = "/dat/";
-			fileName = args[0] + ".pny";
+		arg = args[0];
+		if (arg.length() == 0) {
+			argExists = false;
+			cfg = new Config();
 		}
-		else if (args.length == 2) {
-			isMain = args[0].equals("main");
-			if (args[1].equals("-u")) {
-				filePath = "/dat/test/";
-				fileName = args[0] + ".test";
-				isUnitTest = true;
-			}
-			else if (args[1].equals("-r")) {
-				filePath = "/dat/rt/";
-				fileName = args[0] + ".test";
-				isRunTest = true;
-			}
-			else {
-				System.out.println("Usage: java pny filename [-u][-r]");
-				return;
+		idx = arg.indexOf(sep);
+		if (idx >= 0) {
+			sepWord = arg.substring(idx);
+			fileName = arg.substring(0, idx);
+		}
+		else if (argExists) {
+			fileName = arg;
+		}
+		if (argExists) {
+			// process config block
+			filePath = System.getenv("PNY_HOME");
+			cfg = new Config(filePath, fileName, sepWord);
+			if (!cfg.isValid()) {
+				System.out.println(
+					"Argument entered in command line is invalid."
+				);
+				argExists = false;
 			}
 		}
-		else if (args.length > 2) {
-			System.out.println("Usage: java pny [filename]");
+		if (!argExists) {
+			// display help info...
+			cfg.displayHelpInfo();
 			return;
 		}
+		// config block has been executed
+		isMain = fileName.equals("main");
+		isUnitTest = cfg.isUnitTest();
+		isRunTest = cfg.isRunTest();
+		if (isUnitTest) {
+			fileMidPath = "/dat/test/";
+			fileName = args[0] + ".test";
+		}
+		else if (isRunTest) {
+			fileMidPath = "/dat/rt/";
+			fileName = args[0] + ".test";
+		}
+		else {
+			fileMidPath = "/dat/";
+			fileName = args[0] + ".pny";
+		}
 		initobj = new InitMain(cfg);
-		filePath = System.getenv("PNY_HOME") + filePath;
+		filePath = filePath + fileMidPath;
 		initobj.runInit(filePath, fileName, isUnitTest, isMain, isRunTest);
 	}
-
+	
 }
