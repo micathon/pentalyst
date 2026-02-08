0c79c2f9f60ac33be3977eedc9a089605ebf842e
Mike Hahn <hahnbytes@gmail.com>
2026-02-07 21:30:05 -0500

    Add files via upload

--- src/test/java/test/ConfigTest.java
+++ src/test/java/test/ConfigTest.java
@@ -6,17 +6,88 @@ import config.Config;
 import static org.junit.Assert.*;
 
 public class ConfigTest {
+	
+	String pnyHomeFileName;
+	String outFileName = "/out.txt";
+	String outFullFileName;
+	String sepWord = ".";
 
+	private void initConfigTest() {
+		pnyHomeFileName = System.getenv("PNY_HOME");
+		outFullFileName = pnyHomeFileName + outFileName; 
+	}
+	
 	@Test
 	public void testUseDefaults() {
 		Config cfg = new Config();
 		cfg.useDefaults();
-		assertEquals(cfg.isDebug(), false);
-		assertEquals(cfg.isFileOut(), false);
-		assertEquals(cfg.isUnitTest(), false);
-		assertEquals(cfg.isRunTest(), false);
-		assertEquals(cfg.isCmdPrompt(), false);
-		//assertFalse(cfg.isFileOut());
-		//assertEquals(ex.factorial(4), 24);
+		assertFalse(cfg.isDebug());
+		assertFalse(cfg.isFileOut());
+		assertFalse(cfg.isUnitTest());
+		assertFalse(cfg.isRunTest());
+		assertFalse(cfg.isCmdPrompt());
 	}
+	
+	private Config useSepWord(String sepWord) {
+		Config cfg;
+		initConfigTest();
+		cfg = new Config(pnyHomeFileName, sepWord, outFullFileName);
+		return cfg;
+	}
+
+	@Test
+	public void testConfigNoDot() {
+		Config cfg;
+		String sepWord = "A";
+		int errCode;
+
+		cfg = useSepWord(sepWord);
+		errCode = cfg.badSepErrCode();
+		assertEquals(errCode, 1);
+	}
+
+	@Test
+	public void testConfigSepUR() {
+		Config cfg;
+		String sepWord = ".ur";
+		int errCode;
+
+		cfg = useSepWord(sepWord);
+		errCode = cfg.badSepErrCode();
+		assertEquals(errCode, 2);
+	}
+
+	@Test
+	public void testConfigSepOversize() {
+		Config cfg;
+		String sepWord = ".dfuz";
+		int errCode;
+
+		cfg = useSepWord(sepWord);
+		errCode = cfg.badSepErrCode();
+		assertEquals(errCode, 3);
+	}
+
+	@Test
+	public void testConfigSepDups() {
+		Config cfg;
+		String sepWord = ".dd";
+		int errCode;
+
+		cfg = useSepWord(sepWord);
+		errCode = cfg.badSepErrCode();
+		assertEquals(errCode, 4);
+	}
+
+	@Test
+	public void testConfigSepBadCh() {
+		Config cfg;
+		String sepWord = ".dz";
+		int errCode;
+
+		cfg = useSepWord(sepWord);
+		errCode = cfg.badSepErrCode();
+		assertEquals(errCode, 4);
+	}
+
 }
