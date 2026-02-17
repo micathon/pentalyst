9d54ed289f9f448d13f9ce36b9606d816c90cb79
Mike Hahn <hahnbytes@gmail.com>
2026-02-17 02:02:20 -0500

    Add files via upload

--- src/test/java/test/SynChkStmtTest.java
+++ src/test/java/test/SynChkStmtTest.java
@@ -12,12 +12,18 @@ public class SynChkStmtTest {
 	int modno = 2;
 	String modstr = "c02/";
 
-	private Config runTest(String fileName, int errno) {
+	private Config runTest(String fileName, int miderrno, int errno) {
 		String filePath;
 		String fileMidPath;
 		InitMain initobj;
-		Config cfg = new Config();
+		Config cfg;
+		double errval;
 		
+		cfg = new Config();
+		errval = errno + (miderrno * 100);
+		errval /= 100.0;
+		cfg.setModNo(modno);
+		cfg.setModErrNo(errval);
 		fileName += errno;
 		fileName += ".pny";
 		filePath = System.getenv("PNY_HOME");
@@ -31,11 +37,13 @@ public class SynChkStmtTest {
 	public void doIfStmt(int errno) {
 		int fullerrno;
 		int moderrno;
+		int miderrno;
 		Config cfg;
-		cfg = runTest("doIfStmt", errno);
+		miderrno = 30;
+		cfg = runTest("doIfStmt", miderrno, errno);
 		moderrno = cfg.getModErrNo();
 		fullerrno = cfg.getFullErrNo(modno, moderrno);
-		assertEquals(fullerrno, 203000 + errno);
+		assertEquals(fullerrno, 200000 + (miderrno * 100) + errno);
 	}
 	
 	@Test
@@ -46,6 +54,26 @@ public class SynChkStmtTest {
 	@Test
 	public void doIfStmt20() {
 		doIfStmt(20);
+	}
+	
+	@Test
+	public void doIfStmt30() {
+		doIfStmt(30);
+	}
+	
+	@Test
+	public void doIfStmt40() {
+		doIfStmt(40);
+	}
+	
+	@Test
+	public void doIfStmt50() {
+		doIfStmt(50);
+	}
+	
+	@Test
+	public void doIfStmt60() {
+		doIfStmt(60);
 	}
 	
 }
