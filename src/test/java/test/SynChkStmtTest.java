91faf87d5ed04dd293b03063759b73e91918f075
Mike Hahn <hahnbytes@gmail.com>
2026-02-26 04:17:26 -0500

    Add files via upload

--- src/test/java/test/SynChkStmtTest.java
+++ src/test/java/test/SynChkStmtTest.java
@@ -52,6 +52,11 @@ public class SynChkStmtTest {
 		doSetOpStmt(10);
 	}
 */	
+	@Test
+	public void doSetOpStmt20() {
+		doSetOpStmt(20);
+	}
+
 	private void doSetStmtTail(int errno) {
 		handleStmt("doSetStmtTail", 50, errno);
 	}
@@ -64,6 +69,111 @@ public class SynChkStmtTest {
 	@Test
 	public void doSetStmtTail20() {
 		doSetStmtTail(20);
+	}
+	
+	@Test
+	public void doSetStmtTail30() {
+		doSetStmtTail(30);
+	}
+	
+	private void doSetStmt(int errno) {
+		handleStmt("doSetStmt", 60, errno);
+	}
+	
+	@Test
+	public void doSetStmt10() {
+		doSetStmt(10);
+	}
+	
+	private void doSetTuple(int errno) {
+		handleStmt("doSetTuple", 70, errno);
+	}
+	
+	@Test
+	public void doSetTuple40() {
+		doSetTuple(40);
+	}
+/*	
+	@Test
+	public void doSetTuple50() {
+		doSetTuple(50);
+	}
+*/	
+	private void doIncDecStmt(int errno) {
+		handleStmt("doIncDecStmt", 80, errno);
+	}
+	
+	@Test
+	public void doIncDecStmt10() {
+		doIncDecStmt(10);
+	}
+	
+	@Test
+	public void doIncDecStmt20() {
+		doIncDecStmt(20);
+	}
+	
+	@Test
+	public void doIncDecStmt30() {
+		doIncDecStmt(30);
+	}
+	
+	private void doWhileStmt(int errno) {
+		handleStmt("doWhileStmt", 90, errno);
+	}
+	
+	@Test
+	public void doWhileStmt10() {
+		doWhileStmt(10);
+	}
+	
+	@Test
+	public void doWhileStmt20() {
+		doWhileStmt(20);
+	}
+	
+	@Test
+	public void doWhileStmt30() {
+		doWhileStmt(30);
+	}
+	
+	@Test
+	public void doWhileStmt40() {
+		doWhileStmt(40);
+	}
+	
+	@Test
+	public void doWhileStmt45() {
+		doWhileStmt(45);
+	}
+/*	
+	@Test
+	public void doWhileStmt50() {
+		doWhileStmt(50);
+	}
+	
+	@Test
+	public void doWhileStmt60() {
+		doWhileStmt(60);
+	}
+	
+	@Test
+	public void doWhileStmt70() {
+		doWhileStmt(70);
+	}
+	
+	@Test
+	public void doWhileStmt80() {
+		doWhileStmt(80);
+	}
+*/	
+	private void doForStmt(int errno) {
+		handleStmt("doForStmt", 100, errno);
+	}
+	
+	@Test
+	public void doForStmt10() {
+		doForStmt(10);
 	}
 	
 }
