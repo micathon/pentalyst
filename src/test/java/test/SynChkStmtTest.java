8fd7e9862ab250ccd5c7b9e90734e7c469f8481f
Mike Hahn <hahnbytes@gmail.com>
2026-02-25 21:17:55 -0500

    Add files via upload

--- src/test/java/test/SynChkStmtTest.java
+++ src/test/java/test/SynChkStmtTest.java
@@ -32,15 +32,38 @@ public class SynChkStmtTest {
 	public void doIfStmt40() {
 		doIfStmt(40);
 	}
-	
+/*	
 	@Test
 	public void doIfStmt50() {
 		doIfStmt(50);
 	}
-	
+*/	
 	@Test
 	public void doIfStmt60() {
 		doIfStmt(60);
+	}
+	
+	private void doSetOpStmt(int errno) {
+		handleStmt("doSetOpStmt", 40, errno);
+	}
+/*	
+	@Test
+	public void doSetOpStmt10() {
+		doSetOpStmt(10);
+	}
+*/	
+	private void doSetStmtTail(int errno) {
+		handleStmt("doSetStmtTail", 50, errno);
+	}
+/*	
+	@Test
+	public void doSetStmtTail10() {
+		doSetStmtTail(10);
+	}
+*/	
+	@Test
+	public void doSetStmtTail20() {
+		doSetStmtTail(20);
 	}
 	
 }
