09a86a544935778e88bfe3df9ebdfd1db691ea77
Mike Hahn <hahnbytes@gmail.com>
2026-05-03 20:13:35 -0400

    Add files via upload

--- src/main/java/iconst/BifTyp.java
+++ src/main/java/iconst/BifTyp.java
@@ -1,7 +1,7 @@
 package iconst;
 
 public enum BifTyp {
-	NULL, LEN, RANGE, LYFUNC, CRPATH, CAR, CDR, RPLACA, RPLACD;
+	NULL, LEN, RANGE, LYFUNC, CRPATH, RPLACA, RPLACD; //, CAR, CDR
 	
 	public static final BifTyp values[] = values();
 }
